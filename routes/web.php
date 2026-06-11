4147080eacc61a8d371b2f478f8405cc492fcfaa
Eduardo <edjpereira@gmail.com>
2026-06-11 16:36:39 +0100

    Passos 6 e 7: Criação dos controllers e configuração das rotas

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,27 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClientController;
+use App\Http\Controllers\VehicleController;
+use App\Http\Controllers\SaleController;
 
+// Rota inicial
 Route::get('/', function () {
     return view('welcome');
 });
 
+// Rotas de Autenticação
 Auth::routes();
 
+// Rota do Dashboard
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// Área para utilizadores autenticados
+Route::middleware(['auth'])->group(function () {
+
+    // Rotas do tipo Resource geram automaticamente todas as URLs para o CRUD
+    Route::resource('clients', ClientController::class);
+    Route::resource('vehicles', VehicleController::class);
+    Route::resource('sales', SaleController::class);
+
+});
